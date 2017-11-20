6df1b52a9fdf29db6f33f20ec5233e41caa95bd3
Leon Noel <leon.a.noel@gmail.com>
2017-11-20 13:45:10 -0500

    First - all student code

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// var object = document.getElementById('item');
+//
+// object.onclick=function(){
+//   var x = Math.floor(Math.random()*300);
+//   var y = Math.floor(Math.random()*300);
+//   object.style.top = x + 'px';
+//   object.style.left = y + 'px';
+// };
+
+
+var object = document.getElementById('horses');
+
+object.onclick=function(){
+  var x = Math.floor(Math.random()*300);
+  var y = Math.floor(Math.random()*300);
+  object.style.top = x + 'px';
+  object.style.left = y + 'px';
+};
+
+var duck = document.getElementById('duck');
+
+duck.onclick=function(){
+  var x = Math.floor(Math.random()*300);
+  var y = Math.floor(Math.random()*300);
+  duck.style.top = x + 'px';
+  duck.style.left = y + 'px';
+};
